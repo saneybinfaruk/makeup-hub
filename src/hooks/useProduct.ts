06684f867e19748b5c2ae2ff322d792a1c545089
SaneyBinFaruk <saneybinfaruk@SaneyBinFaruks-iMac.local>
2024-05-08 04:26:34 +0600

    fix: change api to https

--- src/hooks/useProduct.ts
+++ src/hooks/useProduct.ts
@@ -4,7 +4,7 @@ import { Product } from "../pages/HomePage";
 
 const useProduct = (id: string) => {
 
-  const BASE_URL = "http://makeup-api.herokuapp.com/api/v1/products";
+  const BASE_URL = "https://makeup-api.herokuapp.com/api/v1/products";
 
   const axiosInstance = axios.create({
     baseURL: BASE_URL,
